889b70c30028ad5b3c5df3817686742235a0b6d7
egreerdp <ewan.greer@dailypay.com>
2024-08-07 20:45:11 +0100

    feat: init svelt

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,6 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#6b21a8",
-          secondary: "#a78bfa",
-          accent: "#e879f9",
-          neutral: "#1c1917",
-          "base-100": "#111827",
-          info: "#d946ef",
-          success: "#00ff00",
-          warning: "#f59e0b",
-          error: "#ff0000",
-        },
-      },
-    ],
+    themes: ["night", "dark", "sunset", "black"],
   },
 };
